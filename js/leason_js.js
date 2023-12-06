2c983a477858a351532b38636e6ef4e9acaa0fd9
OlehMiliev <olegmilevmmm@gmail.com>
2023-12-06 14:29:58 +0200

    date

--- js/leason_js.js
+++ js/leason_js.js
@@ -1,60 +1,70 @@
-// create obj
-let new_user = {
-  name:"Nick",
-  age: 11,
-  sex:"male",
-  toString(){
-    return `{name: "${this.name}", age: ${this.age}, sex: ${this.sex}`;
-  }
-}
-
-// преобразования объектов в JSON.
- let student = {
-   name: "Alex",
-   age:17,
-   sex:"male",
-   isAdmin: false,
-   courses: ["Programing"],
-   wife:null
- }
- let json_student = JSON.stringify(student);
-
-
-// конвертация обьектов в массив
-
-let meetUp = {
-  title:"Campus",
-  room:{
-   number: 22,
-   neighbours:["Ivy", "Lee"]
-  }
-}
-
-
+// // create obj
+// let new_user = {
+//   name:"Nick",
+//   age: 11,
+//   sex:"male",
+//   toString(){
+//     return `{name: "${this.name}", age: ${this.age}, sex: ${this.sex}`;
+//   }
+// }
+//
+// // преобразования объектов в JSON.
+//  let student = {
+//    name: "Alex",
+//    age:17,
+//    sex:"male",
+//    isAdmin: false,
+//    courses: ["Programing"],
+//    wife:null
+//  }
+//  let json_student = JSON.stringify(student);
+//
+//
+// // конвертация обьектов в массив
+//
+// let meetUp = {
+//   title:"Campus",
+//   room:{
+//    number: 22,
+//    neighbours:["Ivy", "Lee"]
+//   }
+// }
+//
+//
+// let room = {
+//   number: 23
+// };
+//
+// let meetup = {
+//   title: "Conference",
+//   participants: [{name: "John"}, {name: "Alice"}],
+//   place: room
+// };
+//
+// room.occupiedBy = meetup;
+//
+// console.log(JSON.stringify(meetup,function (key,value) {
+//   console.log(`${key}: ${value}`);
+//   return(key == 'occupiedBy') ? undefined:value;
+// }));
+//
+// let user_2 = {
+//   name:"Dodik",
+//   age:22,
+//   roles:{
+//     isAdmin: false,
+//     isEditor: true
+//   }
+// }
+// console.log(JSON.stringify(user_2,null,12));
 let room = {
   number: 23
 };
 
-let meetup = {
-  title: "Conference",
-  participants: [{name: "John"}, {name: "Alice"}],
-  place: room
+let meetUpDate = {
+  title:"Conference",
+  date:new Date(Date.UTC(2022, 11 , 12)),
+  room
 };
-
-room.occupiedBy = meetup;
-
-console.log(JSON.stringify(meetup,function (key,value) {
-  console.log(`${key}: ${value}`);
-  return(key == 'occupiedBy') ? undefined:value;
-}));
-
-let user_2 = {
-  name:"Dodik",
-  age:22,
-  roles:{
-    isAdmin: false,
-    isEditor: true
-  }
-}
-console.log(JSON.stringify(user_2,null,12))
+console.log(JSON.stringify(meetUpDate,null,10));
 
